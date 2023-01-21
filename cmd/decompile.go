f973a537c98b1682644fe08fa31ed2f08065a2c0
AureumApes <janundartur@gmail.com>
2023-01-21 17:16:36 +0100

    Fixed Dependencies

--- cmd/decompile.go
+++ cmd/decompile.go
@@ -8,7 +8,7 @@ import (
 
 func Decompile(fileName string) {
 	fileBytes, _ := os.ReadFile(fileName)
-	fileContent := strings.Split(goutils.Decrypt([]rune("siec"), string(fileBytes)), "\n")
+	fileContent := strings.Split(goutil.Decrypt([]rune("siec"), string(fileBytes)), "\n")
 	instructions := []int64{}
 	for i, _ := range fileContent {
 		instructions = append(instructions, int64(i+1))
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AureumApes/siec
 
 go 1.19
+
+require github.com/AureumApes/goutil master
